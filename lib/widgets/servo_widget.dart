debc8ce3d8b6ba1b3d99ba8d7479c44140b6c82a
Bhashi C <bcwidanagamage@gmail.com>
2023-04-09 22:37:45 +0700

    Created Servo Widget

--- lib/widgets/servo_widget.dart
+++ lib/widgets/servo_widget.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:robot_app/proviers/slider_provider.dart';
+import 'package:robot_app/widgets/slider_widget.dart';
+import 'package:provider/provider.dart';
+import '../proviers/pid_provider.dart';
+import '../proviers/servo_provider.dart';
+
+const List<double> _servoLimits = <double>[0, 30, 60, 90, 120, 150, 180];
 
 class ServoWidget extends StatelessWidget {
-  const ServoWidget({super.key});
-
   @override
   Widget build(BuildContext context) {
-    print("Servo Widget Builder");
-    return Scaffold(
-        appBar: AppBar(
-      title: const Text("Servo Motor Page"),
-    ));
+    //print("Pid Widget Builder");
+    var servo = Provider.of<ServoProvider>(context, listen: false);
+    return ListView.builder(
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      itemCount: 1,
+      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+        value: servo.servoSlider,
+        child: SliderWidget(
+          _servoLimits,
+          servo.servoSlider.minVal,
+          servo.servoSlider.maxVal,
+        ),
+      ),
+    );
   }
 }
